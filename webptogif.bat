97a1917088c95e710fb39a037b5f16d21b1f614c
DetektywNaczos <maciekch3@gmail.com>
2021-09-22 20:52:07 +0200

    grupa

--- webptogif.bat
+++ webptogif.bat
@@ -3,7 +3,7 @@ SET /a a = 1
 x:
 cd X:\BOTY DSC\DetektywNaczos.github.io
 :start
-FbWebp2Gif.exe "X:\BOTY DSC\DetektywNaczos.github.io\szmata\%a%.webp"
-del "X:\BOTY DSC\DetektywNaczos.github.io\szmata\%a%.webp"
+FbWebp2Gif.exe "X:\BOTY DSC\DetektywNaczos.github.io\lesbian\%a%.webp"
+del "X:\BOTY DSC\DetektywNaczos.github.io\lesbian\%a%.webp"
 SET /a a = %a% + 1
 goto :start
